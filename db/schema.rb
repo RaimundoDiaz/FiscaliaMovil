640f1ba6159a014db4b10945a6173b0466608981
rlemayd <ralemay@miuandes.cl>
2020-10-27 19:09:58 -0300

    Logg in with devise first attempt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_001751) do
+ActiveRecord::Schema.define(version: 2020_10_27_214157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -164,8 +164,15 @@ ActiveRecord::Schema.define(version: 2020_09_27_001751) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["local_prosecution_id"], name: "index_users_on_local_prosecution_id"
     t.index ["police_unit_id"], name: "index_users_on_police_unit_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "alias_accuseds", "people"
