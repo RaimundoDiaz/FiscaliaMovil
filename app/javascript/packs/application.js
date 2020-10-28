e3515086869586687114f9b63e0cf7866a10b292
Web Tech <jacastro2@miuandes.cl>
2020-10-28 10:03:18 -0300

    application.js funcionando

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("packs/procedures")
+require("./procedures.js.erb")
 
 //= require jquery3
+//= require jquery_ujs
 //= require popper
 //= require bootstrap
 
