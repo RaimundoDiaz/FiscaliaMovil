9720b50bf2d30bfecdee653d16be964191f61ffe
andresvial <avial5@miuandes.cl>
2020-11-01 11:04:15 -0300

    Finished  form to create new messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 p = Prefecture.create(name: "prefectura")
 ps = PoliceStation.create(prefecture: p)
 pu = PoliceUnit.create(police_station: ps)
