4fd4784398e142aadefc323f77ea16b1113b645e
rlemayd <ralemay@miuandes.cl>
2020-11-03 16:10:33 -0300

    almost everything done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,81 +1,122 @@
 class PagesController < ApplicationController
 
   def search
+    @fin = []
     @palabras_clave = []
     if params[:palabras_clave] != nil and params[:palabras_clave] != ""
+      print("PALABRA CLAVE")
       x = Procedure.where("story LIKE ?", "%"+params[:palabras_clave].to_s+"%").pluck(:id)
       for i in x
         @palabras_clave.push(i)
       end
     end
+    @query = []
+    if params[:query] != nil and params[:query] != ""
+      print("QUERY")
+      x = Procedure.where("story LIKE ?", "%"+params[:query].to_s+"%").pluck(:id)
+      for i in x
+        @query.push(i)
+      end
+    end
     @desde = []
     if params[:desde] != nil and params[:desde] != "" and params[:hasta] != nil and params[:hasta] != ""
+      print("DESDE Y HASTA")
       x = Procedure.where("DATE(created_at) > ? AND DATE(created_at) < ?", params[:desde], params[:hasta]).pluck(:id)
       for i in x
-        @palabras_clave.push(i)
+        @desde.push(i)
       end
     elsif params[:desde] != nil and params[:desde] != ""
+      print("DESDE")
       x = Procedure.where("DATE(created_at) > ?", params[:desde]).pluck(:id)
       for i in x
-        @palabras_clave.push(i)
+        @desde.push(i)
       end
     elsif params[:hasta] != nil and params[:hasta] != ""
+      print("HASTA")
       x = Procedure.where("DATE(created_at) < ?", params[:hasta]).pluck(:id)
       for i in x
-        @palabras_clave.push(i)
+        @desde.push(i)
       end
     end
 
-
+    @clasificacion = []
     if params[:classification] != nil and params[:classification] != ""
+      print("CLASIFICAION: ", params[:classification] )
       case params[:classification]
       when "Flagrancia"
         x = Procedure.where("classification = 0").pluck(:id)
         for i in x
-          @palabras_clave.push(i)
+          @clasificacion.push(i)
         end
       when "ODP"
         x = Procedure.where("classification = 1").pluck(:id)
         for i in x
-          @palabras_clave.push(i)
+          @clasificacion.push(i)
         end
       when "Ambas"
         x = Procedure.where("classification = 2").pluck(:id)
         for i in x
-          @palabras_clave.push(i)
+          @clasificacion.push(i)
         end
       end
     end
 
-
-    if params[:involucra_fallecidos] != nil and params[:classification] != ""
+    @involucra_fallecidos = []
+    if params[:involucra_fallecidos] != nil and params[:involucra_fallecidos] != ""
+      print("involucra_fallecidos")
       case params[:involucra_fallecidos]
-      when "1"
+      when "Si"
         x = Procedure.where("involves_deceased = true").pluck(:id)
         for i in x
-          @palabras_clave.push(i)
+          @involucra_fallecidos.push(i)
         end
       else
         x = Procedure.where("involves_deceased = false").pluck(:id)
         for i in x
-          @palabras_clave.push(i)
+          @involucra_fallecidos.push(i)
         end
       end
     end
 
-
+    @delito = []
     if params[:delito] != nil and params[:delito] != ""
+      print("delito")
       x = Procedure.find(CrimeInAccused.where("crime_id = ?",Crime.where(name: params[:delito]).pluck(:id)).pluck(:procedure_id)).pluck(:id)
       for i in x
-        @palabras_clave.push(i)
+        @delito.push(i)
       end
     end
 
     #if params[:marca] != nil and params[:marca] != ""
     #  @query_buscada.push(Procedure.find(CrimeInAccused.where("crime_id = ?",Crime.where(name: params[:marca]).pluck(:id)).pluck(:procedure_id)))
     #end
-    @palabras_clave = @palabras_clave.uniq
-    @procedimientos_buscados = Procedure.find(@palabras_clave)
+    for i in [@query, @palabras_clave, @desde, @clasificacion, @involucra_fallecidos, @delito]
+      if i != []
+        if @palabras_clave == []
+          @palabras_clave = i
+        end
+        if @desde == []
+          @desde = i
+        end
+        if @clasificacion == []
+          @clasificacion = i
+        end
+        if @involucra_fallecidos == []
+          @involucra_fallecidos = i
+        end
+        if @delito == []
+          @delito = i
+        end
+        if @query == []
+          @query = i
+        end
+        break
+      end
+    end
+
+    @fin = @palabras_clave & @desde & @clasificacion & @involucra_fallecidos & @delito & @query
+    @procedimientos_buscados = Procedure.find(@fin)
+
   end
 
 end
