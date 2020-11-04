e0fa2bad1d56fe6b496010887ced81727eb3515e
Web Tech <jacastro2@miuandes.cl>
2020-11-04 02:32:24 -0300

    done v5

--- db/seeds.rb
+++ db/seeds.rb
@@ -58,8 +58,8 @@ u1 = User.create(police_unit_id: 10101, email: "unidadpolicia1@gmail.com", passw
 u2 = User.create(local_prosecution_id: 102, email: "fiscalia1@gmail.com", password: "123456789")
 police = PoliceMan.create(name: "Jonathan Castro", rut: "19638546-k", badge:1524352, police_unit_id: 10101)
 prosecutor = Prosecutor.create(name: "Rodrigo Ponce", rut: "19638846-k", local_prosecution_id: 102)
-procedure1 = Procedure.create(story: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", state: 0, address: "Alicura 4339", sector: "Lo Barnechea", region: "Metropolitana", classification: 0, police_unit_in_charge_id: 10101, police_in_charge: police, local_prosecution_in_charge_id: 103, prosecutor_in_charge: prosecutor, involves_deceased: false)
-procedure2 = Procedure.create(story: "HOLAAAA Sed", state: 0, address: "Alicura 4339", sector: "Lo Barnechea", region: "Metropolitana", classification: 1, police_unit_in_charge_id: 10101, police_in_charge: police, local_prosecution_in_charge_id: 103, prosecutor_in_charge: prosecutor, involves_deceased: false)
+procedure1 = Procedure.create(story: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", state: 0, address: "Monseñor Álvaro del Portillo 123456", sector: "Las Condes", region: "Metropolitana", date_of_arrest: Date.today ,classification: 0, police_unit_in_charge_id: 10101, police_in_charge: police, local_prosecution_in_charge_id: 103, prosecutor_in_charge: prosecutor, involves_deceased: false)
+procedure2 = Procedure.create(story: "Robo con intimidacion", state: 0, address: "Alicura 4339", sector: "Lo Barnechea", region: "Metropolitana", date_of_arrest: Date.yesterday, classification: 1, police_unit_in_charge_id: 10101, police_in_charge: police, local_prosecution_in_charge_id: 103, prosecutor_in_charge: prosecutor, involves_deceased: false)
 person = Person.create(name: "Juan", last_name: "Perez", rut: "19838173-k", deceased: false, birthday: Date.new(2000))
 imputado = Person.create(name: "Martin", last_name: "Moreno", rut: "19838173-k", deceased: true, birthday: Date.new(1997))
 imputado2 = Person.create(name: "Gian", last_name: "Traverso", rut: "19687033-4", deceased: false, birthday: Date.new(1997))
@@ -71,13 +71,23 @@ CrimeInAccused.create(person: imputado, crime_id: 310, procedure: procedure1, pr
 msg1 = Message.create(user: u1, procedure: procedure1, content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.")
 msg2 = Message.create(user: u1, procedure: procedure1, content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.")
 msg3 = Message.create(user: u2, procedure: procedure1, content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.")
+
+
 tag1 = Tag.create(name: "Estallido Social")
 tag2 = Tag.create(name: "DS18")
 tag3 = Tag.create(name: "VIF")
+tag4 = Tag.create(name: "Vehicular")
+tag5 = Tag.create(name: "Robo")
+tag6 = Tag.create(name: "Disturbios")
+tag7 = Tag.create(name: "Narcotráfico")
+tag8 = Tag.create(name: "Persecusión")
+tag9 = Tag.create(name: "Asalto")
+tag10 = Tag.create(name: "Heridos")
+
+
 Tagging.create(tag: tag1, procedure: procedure1)
 Tagging.create(tag: tag3, procedure: procedure1)
 Tagging.create(tag: tag2, procedure: procedure1)
-Tagging.create(tag: tag2, procedure: procedure2)
-Tagging.create(tag: tag3, procedure: procedure1)
-Tagging.create(tag: tag3, procedure: procedure2)
-
+Tagging.create(tag: tag5, procedure: procedure2)
+Tagging.create(tag: tag6, procedure: procedure1)
+Tagging.create(tag: tag9, procedure: procedure2)
