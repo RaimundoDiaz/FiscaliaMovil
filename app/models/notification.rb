3692ec88873f43bec9ec2fda638886cb3c343d28
andresvial <avial5@miuandes.cl>
2020-11-19 15:57:28 -0300

    minor fixes

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,10 +1,10 @@
 class Notification < ApplicationRecord
   belongs_to :user
 
-  #0: Llego procedimiento creado al fiscal asignado (a fiscal)
-  #1: Fiscal cerró un procedimiento (a carabinero)
-  #2: Fiscal solicito más datos respecto a un procedimiento (a carabinero)
-  #3: Carabinero respondió con más datos respecto a un procedimiento (a fiscal)
+  #0: Llego un nuevo procedimiento (pendiente)
+  #1: Fiscal cerró un procedimiento (pendiente)
+  #2: Fiscal solicito más datos respecto a un procedimiento
+  #3: Carabinero respondió con más datos respecto a un procedimiento
   #4: Has recibido un mensaje
 
   enum notification_type: {new_procedure: 0, procedure_closed: 1, procedure_requiring_info: 2, procedure_info_updated: 3, new_message: 4}
