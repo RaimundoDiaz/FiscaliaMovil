c93476665bf360af5b1138e241ccdc92d9546480
andresvial <avial5@miuandes.cl>
2020-11-22 16:24:16 -0300

    Added after_create of users by creating police_units

--- app/models/police_unit.rb
+++ app/models/police_unit.rb
@@ -1,5 +1,18 @@
 class PoliceUnit < ApplicationRecord
+  after_create :create_user
   belongs_to :police_station
   has_many :users
   has_many :procedures
+
+  def create_user
+    #Email
+    @email = "up"+self.id.to_s+"@fiscalia.com"
+
+    puts(@email)
+
+    #Password
+    @password = 123456789
+
+    User.create(police_unit: self, email: @email, password: "123456789")
+  end
 end
