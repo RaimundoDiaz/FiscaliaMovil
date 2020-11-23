7ea77765898b9238dbdb0eef0fcecfc780be957e
track-tor <jacastro2@miuandes.cl>
2020-11-22 21:26:01 -0300

    arreglado error al inicializar las comunas en el formulario

--- app/javascript/packs/procedures.js
+++ app/javascript/packs/procedures.js
@@ -423,12 +423,17 @@ $(document).on('turbolinks:load', function() {
         }
     ];
 
-    var comunas = regiones[0];
-    comunas = JSON.parse(JSON.stringify(comunas));
-    comunas = comunas.comunas; // [{codigo : 1231, nombre: "", ...}]
+    regiones = JSON.parse(JSON.stringify(regiones));
+    var option = $("#procedure_region").val();
+    var nuevas_comunas = $.grep(regiones, function(region) { return region.codigo == option; });
+    nuevas_comunas = nuevas_comunas[0].comunas;
+    console.log(nuevas_comunas);
+
+    //borrar las opciones actuales
+    $('#procedure_sector').children().remove();
 
     //agregar al select de comunas los datos del json
-    $.each(comunas, function (i, item) {
+    $.each(nuevas_comunas, function (i, item) {
         $('#procedure_sector').append($('<option>', {
             value: item.codigo,
             text : item.nombre
@@ -438,7 +443,6 @@ $(document).on('turbolinks:load', function() {
     //preparar lo anterior para cada vez que se cambie el select de regiones
     $("#procedure_region").change(function(){
         //seleccionar las comunas de la region correspondiente
-        regiones = JSON.parse(JSON.stringify(regiones));
         var option = $(this).val();
         console.log("opcion = " + option);
         var nuevas_comunas = $.grep(regiones, function(region) { return region.codigo == option; });
