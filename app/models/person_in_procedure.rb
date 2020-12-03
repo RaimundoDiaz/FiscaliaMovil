68519eadbdc8f97bbba3603ed22262a9eabbc78b
andresvial <avial5@miuandes.cl>
2020-12-03 19:09:24 -0300

    minor fix

--- app/models/person_in_procedure.rb
+++ app/models/person_in_procedure.rb
@@ -1,7 +1,7 @@
 class PersonInProcedure < ApplicationRecord
   belongs_to :person
   belongs_to :procedure
-  has_one :prosecutor_pronounced, class_name: 'Prosecutor'
+  belongs_to :prosecutor_pronounced, class_name: 'Prosecutor'
   #para el imputado
   enum state: {pending: 0, acd: 1, noticed: 2}
 end
